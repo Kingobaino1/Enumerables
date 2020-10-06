fa703606c010d60d36293d26fa952a81caef315e
bigwizzo <wnnwillard@gmail.com>
2020-10-06 22:09:18 +0200

    Added the my_each_with_index method

--- enumerables.rb
+++ enumerables.rb
@@ -7,12 +7,30 @@ module Enumerable
       if block_given?
         yield array[i]
       else
-        return "\#<Enumerator: #{self}:each>"
+        return "\#<Enumerator: #{self}:my_each>"
       end
       i += 1
     end
     self
   end
+
+  def my_each_with_index
+    array = self if self.class == Array
+    array = self.to_a if self.class == Range
+    value = 0
+    while value < array.length do
+      if block_given?
+        yield(array[value],value)
+      else
+        return "\#<Enumerator: #{self}:my_each_with_index>"
+      end
+      value += 1
+    end
+    array
+  end
+end
+b = [1,2,3,3,7].my_each_with_index do |value, index|
+ p value => index
 end
 
 arr = [1,2,3,4]
