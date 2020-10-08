655f8309e688243caeb9d0631184af8d07dc5835
Kingsley <kingobaino@gmail.com>
2020-10-08 22:44:29 +0100

    Effect changes pointed out but reviewer

--- enumerables.rb
+++ enumerables.rb
@@ -49,25 +49,35 @@ module Enumerable
     array
   end
 
-  def my_all?(*args)
-    if args.length.zero?
-      i = 0
-      while i < self.length
-        if block_given?
-          if yield(self[i]) == false
-            return false
-          end
-        elsif self[i].nil?
-          return false
-        else
-          return true
-        end
-        i += 1
-      end
-    else
-      return false
-    end
-    true
+ def my_all?(*args)
+   array = self if self.class == Array
+   array = self.to_a if self.class == Range
+   i = 0
+   while i < array.size
+     if args.size.zero?
+       if block_given? && !(yield array[i])
+         return false
+       elsif array[i].nil? || !array[i]
+         return false
+       else
+         return true
+       end
+     elsif args[0] == Float && args[0]!= array[i][i].class
+       return false
+      elsif args[0].class == Regexp && array[i][i].match(args[0]).nil?
+       return false
+     elsif args[0] == Integer && args[0] != array[i][i].class
+       return false
+     elsif args[0] == Numeric && args[0] != (array[i][i].class).superclass
+       return false
+      elsif (args[0] != Float && args[0] != Integer && args[0] != Numeric && args[0].class != Regexp) && args[0] != array[i]
+       return false
+     else
+       return true
+     end
+     i += 1
+   end
+   true
   end
 
   def my_any?(*args)
@@ -154,54 +164,77 @@ module Enumerable
   end
 
   def my_map(my_proc = nil)
+    array = self if self.class == Array
+    array = self.to_a if self.class == Range 
     result = []
     i = 0
-    while i < self.length
-      if block_given?
-        result.push yield self[i]
-      else
-        result.push my_proc.call(self[i])
-      end
+    while i < array.length
+          if block_given? && my_proc != nil
+          result.push my_proc.call(array[i])
+        elsif my_proc.nil?
+          result.push yield array[i]
+       else
+         return to_enum(:my_map)
+       end
       i += 1
     end
     result
   end
 
-  def my_inject(my_proc = nil)
+  def my_inject(my_procs = nil, my_proc = nil)
+    array = self if self.class == Array
+    array = self.to_a if self.class == Range 
     reduce = nil
-    first_element = self.shift
-    self.unshift(first_element)
+    first_element = array.shift
+    array.unshift(first_element)
     i = 1
-    while i < self.length
+    while i < array.length
       if block_given?
-        block_value = yield first_element, self[i]
+        block_value = yield first_element, array[i]
         reduce = block_value
         first_element = reduce
       elsif my_proc == :+
-        proc_symbol = first_element + self[i]
+        proc_symbol = first_element + array[i]
         reduce = proc_symbol
         first_element = reduce
       elsif my_proc == :*
-        proc_symbol = first_element * self[i]
+        proc_symbol = first_element * array[i]
         reduce = proc_symbol
         first_element = reduce
       elsif my_proc == :-
-        proc_symbol = first_element - self[i]
+        proc_symbol = first_element - array[i]
         reduce = proc_symbol
         first_element = reduce
       elsif my_proc == :/
-        proc_symbol = first_element / self[i]
+        proc_symbol = first_element / array[i]
         reduce = proc_symbol
         first_element = reduce
-      end
-      i += 1
+    
     end
-    reduce
+      i += 1
+    end 
+    if my_procs == "" && my_proc == :+
+     return my_procs.to_i + my_proc
+    end
+    reduce + my_procs.to_i
   end
 end
 
 def multiply_els(arr)
   arr.my_inject { |result, element| result * element }
 end
-
+# p %w[ant bear cat].my_all? { |word| word.length >= 3 } #=> true
+# p %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
+# p %w[ant bear cat].my_all?(/a/)                        #=> false
+# p [1, 2, 3.0].my_all?(Integer)                       #=> true
+# p [nil, true, 99].my_all?                              #=> false
+# p [].my_all?{|num| num == 0}   
+arr = [1,2,3,4]
+                                    #=> true
+my_proc = proc {|num| num * 2}
+my_procs = arr.my_map( &my_proc )
+my_procs = arr.my_map {|num| num ** 2}   
+# my_procs = arr.my_map(&my_proc) {|num| num ** 2}  
+p (1..5).my_inject(:+)
+ p my_procs
 # rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/MethodLength, Style/RedundantSelf, Style/GuardClause, Style/IfUnlessModifier, Metrics/BlockNesting, Metrics/ModuleLength
