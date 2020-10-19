68792d8c4d14520a88d9bc14ec794d3a3074a444
Kingsley <kingobaino@gmail.com>
2020-10-19 17:55:25 +0100

    Add test for my_count method and my_map method

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -4,6 +4,7 @@ describe Enumerable do
   let(:array) { [1, 2, 3, 4, 5] }
   let(:range) { (0..5) }
   let(:hash) { { a: 1, b: 2, c: 3 } }
+  let(:my_proc) { proc { |num| num + 1 } }
   describe '#my_each' do
     it 'does not mutate array' do
       expect(array.my_each { |num| num > 2 }).to eql(array)
@@ -58,7 +59,7 @@ describe Enumerable do
     end
   end
 
-  describe "#my_all" do
+  describe '#my_all' do
     it 'return true when all value on the array accomplish condition' do
       expect(array.my_all? {|n| n > 2}).to eql FALSE
     end
@@ -84,7 +85,7 @@ describe Enumerable do
     end
   end
 
-  describe "#my_any?" do
+  describe '#my_any?' do
     it 'return true when any value on the array accomplish condition' do
       expect(array.my_any? {|n| n > 2}).to eql TRUE
     end
@@ -110,7 +111,7 @@ describe Enumerable do
     end
   end
 
-  describe "#my_none?" do
+  describe '#my_none?' do
     it 'return false when none value on the array accomplish condition' do
       expect(array.my_none? {|n| n > 2}).to eql FALSE
     end
@@ -133,6 +134,46 @@ describe Enumerable do
 
     it 'return true when any value on the hash accomplish condition' do
       expect(hash.my_none? {|k, v| v > 6}).to eql TRUE
+    end
+  end
+
+  describe '#my_count' do
+    it 'returns the length of the array if no block and no argument are given' do 
+      expect(array.my_count).to eql(5)
+    end
+
+    it 'returns the number of elements in a range if no block and no argument are given' do 
+      expect(range.my_count).to eql(6)
+    end
+
+    it 'returns the number of elements in a hash if no block and no argument are given' do 
+      expect(hash.my_count).to eql(3)
+    end
+
+    it 'returns the number of elements satisfying the block condition' do 
+      expect(array.my_count { |num| num == 2 }).to eql(1)
+    end
+
+    it 'returns the number of elements that are equal to the argument value' do 
+      expect(array.my_count(1)).to eql(1)
+    end
+  end
+
+  describe '#my_map' do
+    it 'returns a new array for any given operation on each element of an array' do
+      expect(array.my_map { |num| num * 2 }).to eql([2, 4, 6, 8, 10])
+    end
+
+    it 'returns a new array for any given operation on each element of a range' do
+      expect(range.my_map { |num| num * 2 }).to eql([0, 2, 4, 6, 8, 10])
+    end
+
+    it 'returns Enumerator when block is not given' do
+      expect(array.my_map).to be_an(Enumerator)
+    end
+
+    it 'accepts a proc' do
+      expect(array.my_map(&my_proc)).to eql([2, 3, 4, 5, 6])
     end
   end
 end
